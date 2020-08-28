f649934465debdfc441d961b121d350f87ea092d
AlejandroMcno <55595942+AlejandroMcno@users.noreply.github.com>
2020-08-28 12:16:54 -0500

    Create Marca.java

---
+++ src/main/java/taller3/televisores/Marca.java
@@ -0,0 +1,22 @@
+package taller3.televisores;
+
+public class Marca {
+	
+	String nombre;
+	
+	public void Marca(String nombre) {
+		
+		this.nombre=nombre;
+	}
+	
+	public String getMarca() {
+		
+		return nombre;
+	}
+	
+	public String setMarca(String nombre) {
+		
+		this.nombre=nombre;
+	}
+
+}
